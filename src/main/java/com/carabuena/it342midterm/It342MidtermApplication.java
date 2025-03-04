c8f9422ee605e2fd69995b3042844446d951c9af
Paulo Carabuena <45958147+pawekz@users.noreply.github.com>
2025-03-04 11:11:04 +0800

    Initial commit: Set up Spring Boot application with necessary dependencies (Spring Web, Security, OAuth2, Thymeleaf) for Google Contacts API integration. Basic project structure established.

---
+++ src/main/java/com/carabuena/it342midterm/It342MidtermApplication.java
@@ -0,0 +1,13 @@
+package com.carabuena.it342midterm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class It342MidtermApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(It342MidtermApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carabuena/it342midterm/It342MidtermApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carabuena.it342midterm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class It342MidtermApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
