57460035723c4bf89aa74a8749fbd10916e32c51
Paulo Carabuena <45958147+pawekz@users.noreply.github.com>
2025-03-05 15:57:19 +0800

    Update HomeController to use OAuth2User instead of OidcUser for authentication (this is in relation of removing the `openid` scope in application.properties
    it should properly display the logged in user

--- src/main/java/com/carabuena/it342midterm/controller/HomeController.java
+++ src/main/java/com/carabuena/it342midterm/controller/HomeController.java
@@ -1,22 +1,22 @@
 package com.carabuena.it342midterm.controller;
 
-        import org.springframework.security.core.annotation.AuthenticationPrincipal;
-        import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
-        import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
-        import org.springframework.security.oauth2.core.oidc.user.OidcUser;
-        import org.springframework.stereotype.Controller;
-        import org.springframework.ui.Model;
-        import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
-        @Controller
-        public class HomeController {
+@Controller
+public class HomeController {
 
-            @GetMapping("/")
-            public String showIndex(@AuthenticationPrincipal OidcUser principal, Model model) {
-                String name = (principal != null && principal.getAttributes().containsKey("name"))
-                        ? principal.getAttributes().get("name").toString()
-                        : "Unknown User";
-                model.addAttribute("fullName", name);
-                return "index";
-            }
-        }
+    @GetMapping("/")
+    public String showIndex(@AuthenticationPrincipal OAuth2User principal, Model model) {
+        String name = (principal != null && principal.getAttributes().containsKey("name"))
+                ? principal.getAttributes().get("name").toString()
+                : "Unknown User";
+        model.addAttribute("fullName", name);
+        return "index";
+    }
+}
